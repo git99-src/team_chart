48028afd6a988832a2a1193f930c92fabbffd78f
git99.src <cpu.al2@gmail.com>
2020-10-06 23:15:49 -0700

    Completed dynamic file create, read me

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # team_chart
-This application will generate an employee team chart with employee information
+
+Walkthrough video:
+https://drive.google.com/file/d/18x1VcOUVVek1-tTLERGPzCgFmB8HtFn5/view?usp=sharing
+
+
+
+## Description 
+    
+This application will generate an employee team chart with employee information based on user prompted data.
+    
+## Table of Contents 
+          
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Test](#tests)
+  * [Questions](#questions)
+      
+      
+## Installation
+      
+This application created using VS code, Node JS v12, Inquirer JS npm, Jest js npm and FS writeFile. Once Node JS is installed, you will need to run the "npm install" from the terminal.  It will use the package.json included to install Inquirer, jest and dependencies.
+     
+
+## Usage 
+      
+From the terminal command line in VS Code, the user can run the command - node app.js.  This CLI application will then prompt the user for employee information based on the team member type they wish to add - Manager, Engineer or Intern.  The application will then use the answers to the prompts to dynamically build a web page with the Team and their information.  The team.html page will be created in the root "output" folder which will also be created if it does not exist.
+      
+      
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) MIT License
+
+      
+## Contributing
+      
+Please fork the repository in Github with permission.
+
+      
+## Tests
+      
+Testing can be done using the terminal and node js.  Included tests are in the root "test" folder.  The tests check if the constructor objects are created correctly.  To run these tests, type command npm run test.
+
+
+## Questions
+
+Please reach me here if you have any questions -
+Github: git99-src
+      
+
+
+
+      
+      
+---
--- app.js
+++ app.js
@@ -4,78 +4,100 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
+
 
 
 const render = require("./lib/htmlRenderer");
 
+let employeeArray = [];
+
+const managerQuestions = [
+    {
+        name: "name",
+        type: "input",
+        message: "What is your manager's name?"
+    },
+    {
+        name: "id",
+        type: "input",
+        message: "What is your manager's id?"
+    },
+    {
+        name: "email",
+        type: "input",
+        message: "What is your manager's email?"
+    },
+    {
+        name: "officeNumber",
+        type: "input",
+        message: "What is your manager's office number?"
+    },
+    {
+        type: "list",
+        message: "Which type of team member would you like to add:",
+        name: "employeeType",
+        choices: [
+            "Engineer",
+            "Intern",
+            "I don't want to add any more team members",
+        ],
+    },
+]
+const engineerQuestions = [
+    {
+        name: "name",
+        type: "input",
+        message: "What is your engineer's name?"
+    },
+    {
+        name: "id",
+        type: "input",
+        message: "What is your engineer's id?"
+    },
+    {
+        name: "email",
+        type: "input",
+        message: "What is your engineer's email?"
+    },
+    {
+        name: "github",
+        type: "input",
+        message: "What is your engineer's Github username?"
+    },
+    {
+        type: "list",
+        message: "Which type of team member would you like to add:",
+        name: "employeeType",
+        choices: [
+            "Engineer",
+            "Intern",
+            "I don't want to add any more team members",
+        ],
+    },
+]
 main();
 
-async function main() {
-    try {
-        const data = await getUserInput();
-        const htmlString = render(data);
-        await makeOutputFile(htmlString);
-        console.log("Successfully created the HTML");
-    } catch (error) {
-        console.log(error);
-    };
-};
+function main() {
+    console.log("Please build your team")
+    const askManager = inquirer.prompt(managerQuestions).then((answers) => {
+        const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+        employeeArray.push(newManager)
+        console.log("done")
+        const htmlString = render(employeeArray);
+        const outputFilePath = writeToFile("team.html", htmlString);
+        console.log("`Created the HTML file");
+    })
 
-async function getUserInput() {
-    return inquirer.prompt([
-        {
-            name: "name",
-            type: "input",
-            message: "What is your manager's name?"
-        },
-        {
-            name: "id",
-            type: "input",
-            message: "What is your manager's id?"
-        },
-        {
-            name: "email",
-            type: "input",
-            message: "What is your manager's email?"
-        },
-        {
-            name: "officeNumber",
-            type: "input",
-            message: "What is your manager's office number?"
-        },
-    ]);
 };
 
 
-async function makeOutputFile(htmlString) {
-    const OUTPUT_DIR = path.resolve(__dirname, "output");
-    const outputPath = path.join(OUTPUT_DIR, "team.html");
-    if (!fs.existsSync(OUTPUT_DIR)) {
-        await mkdir(OUTPUT_DIR);
-    };
-    await writeFile(outputPath, htmlString);
-};
+function writeToFile(fileName, htmlString) {
+    let outputPath = path.join(process.cwd(), "./output");
+    if (!fs.existsSync(outputPath)) {
+        fs.mkdirSync(outputPath);
+    }
+    outputPath = path.join(outputPath, fileName);
+    fs.writeFileSync(outputPath, htmlString);
+    return outputPath;
+}
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
